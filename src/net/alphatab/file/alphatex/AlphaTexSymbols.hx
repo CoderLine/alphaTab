10801b10176772fa211cd6b214252832d69e201f
Danielku15 <danielku15@coderline.net>
2010-08-10 19:43:59 +0000

    adding alphatex parser
    git-svn-id: http://alphatab.googlecode.com/svn/trunk@15 273ee68d-d738-05ed-89cf-3e312ccb727e

---
+++ src/net/alphatab/file/alphatex/AlphaTexSymbols.hx
@@ -0,0 +1,21 @@
+package net.alphatab.file.alphatex;
+
+/**
+ * A list of terminals recognized by the alphaTex-parser
+ */
+enum AlphaTexSymbols 
+{
+	No;
+	Eof;
+	Number;
+	Dot;
+	Version;
+	String;
+	Tuning;
+	LParensis;
+	RParensis;
+	LBrace;
+	RBrace;
+	Pipe;
+	MetaCommand;
+}
