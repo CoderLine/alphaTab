327b59ded869a97bc66095b371c6bfbd1db8f1f2
Danielku15 <Danielku15@CoderLine.net>
2012-04-23 20:25:46 +0300

    Added GlyphGroup for rendering multiple glyphs at the same position

---
+++ src/alphatab/rendering/glyphs/GlyphGroup.hx
@@ -0,0 +1,56 @@
+/*
+ * This file is part of alphaTab.
+ *
+ *  alphaTab is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  alphaTab is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with alphaTab.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package alphatab.rendering.glyphs;
+
+import alphatab.platform.ICanvas;
+import alphatab.rendering.Glyph;
+
+/**
+ * This glyph allows to group several other glyphs to be
+ * drawn at the same x position
+ */
+class GlyphGroup extends Glyph
+{
+	private var _glyphs:Array<Glyph>;
+	public function new(x:Int = 0, y:Int = 0, glyphs:Array<Glyph>)
+	{
+		super(x, y);
+		_glyphs = glyphs;
+	}
+	
+	public override function doLayout():Void 
+	{
+		var w = 0;
+		for (g in _glyphs)
+		{
+			g.x = 0;
+			g.y = 0;
+			g.renderer = renderer;
+			g.doLayout();
+			w = Math.max(w, g.width);
+		}	
+		width = w;
+	}
+	
+	public override function paint(cx:Int, cy:Int, canvas:ICanvas):Void 
+	{
+		for (g in _glyphs)
+		{
+			g.paint(cx + x, cy + y, canvas);
+		}
+	}
+}
