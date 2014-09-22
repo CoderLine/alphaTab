6e740d01eee2b39593e29ecea851e115c553f84b
Danielku15 <danielku15@coderline.net>
2014-09-23 00:08:07 +0200

    Added Copyright notices to sources

--- Samples/CSharp/AlphaTab.Wpf.Share/Data/IErrorService.cs
+++ Samples/CSharp/AlphaTab.Wpf.Share/Data/IErrorService.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * This file is part of alphaTab.
- * Copyright c 2013, Daniel Kuschny and Contributors, All rights reserved.
+ * Copyright (c) 2014, Daniel Kuschny and Contributors, All rights reserved.
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
--- Samples/CSharp/AlphaTab.Wpf.Share/ViewModel/MainViewModel.cs
+++ Samples/CSharp/AlphaTab.Wpf.Share/ViewModel/MainViewModel.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * This file is part of alphaTab.
- * Copyright c 2013, Daniel Kuschny and Contributors, All rights reserved.
+ * Copyright (c) 2014, Daniel Kuschny and Contributors, All rights reserved.
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library.
  */
-
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
