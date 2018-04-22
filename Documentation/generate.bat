27ed82c737bac721c4687a2a58bc2828e20a1657
Danielku15 <danielku15@coderline.net>
2018-04-22 18:03:26 +0200

    Adjusted directory for documentation script

--- Documentation/generate.bat
+++ Documentation/generate.bat
@@ -1,8 +1,12 @@
 @echo off
 
+pushd %~dp0
+
 SET WYAM=%USERPROFILE%\.nuget\packages\wyam\1.4.1\tools\net462\Wyam.exe
 git rev-parse HEAD > head.txt
 set /p GitRevision=<head.txt
 del head.txt
 for /F "tokens=3 delims=/" %%i in (..\.git\HEAD) do SET GitBranch=%%i
 %WYAM% build --setting GitRevision=%GitRevision% --setting GitBranch=%GitBranch%
+
+popd
