ffb2540f8742849f77cbe75d7dfa61c73de06b32
Danielku15 <danielku15@coderline.net>
2018-04-22 17:58:12 +0200

    Reenabled temporarily disabled documentation generation

--- Documentation/generate.bat
+++ Documentation/generate.bat
@@ -1,10 +1,8 @@
 @echo off
 
-echo %~dp0..\Test\TestFiles\
-
-rem SET WYAM=%USERPROFILE%\.nuget\packages\wyam\1.4.1\tools\net462\Wyam.exe
-rem git rev-parse HEAD > head.txt
-rem set /p GitRevision=<head.txt
-rem del head.txt
-rem for /F "tokens=3 delims=/" %%i in (..\.git\HEAD) do SET GitBranch=%%i
-rem %WYAM% build --setting GitRevision=%GitRevision% --setting GitBranch=%GitBranch%
+SET WYAM=%USERPROFILE%\.nuget\packages\wyam\1.4.1\tools\net462\Wyam.exe
+git rev-parse HEAD > head.txt
+set /p GitRevision=<head.txt
+del head.txt
+for /F "tokens=3 delims=/" %%i in (..\.git\HEAD) do SET GitBranch=%%i
+%WYAM% build --setting GitRevision=%GitRevision% --setting GitBranch=%GitBranch%
