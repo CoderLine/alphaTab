d17160da72be4a992cf94df72ecc258e3d73ee19
Danielku15 <danielku15@coderline.net>
2018-08-18 18:30:00 +0200

    fix syntax error in documentation generation

--- Documentation/generate.bat
+++ Documentation/generate.bat
@@ -2,15 +2,13 @@
 
 pushd %~dp0
 
-IF "%APPVEYOR_REPO_BRANCH%"=="" ( 
+SET GitBranch=%APPVEYOR_REPO_BRANCH%
+IF "%GitBranch%"=="" ( 
 	SET WYAM=%USERPROFILE%\.nuget\packages\wyam\1.4.1\tools\net462\Wyam.exe
 	git rev-parse HEAD > head.txt
 	set /p GitRevision=<head.txt
 	del head.txt
 	for /F "tokens=3 delims=/" %%i in (..\.git\HEAD) do SET GitBranch=%%i
-)
-ELSE (
-	SET GitBranch=%APPVEYOR_REPO_BRANCH%
 )
 
 rmdir /S /Q input\assets\js\alphaTab
