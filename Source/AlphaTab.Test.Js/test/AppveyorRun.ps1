c4eb6f497da02f3bce242ec701732f920e0e5934
Daniel Kuschny <danielku15+gitkraken@gmail.com>
2018-11-04 21:41:45 +0100

    Small extension on testrunner script

--- Source/AlphaTab.Test.Js/test/AppveyorRun.ps1
+++ Source/AlphaTab.Test.Js/test/AppveyorRun.ps1
@@ -1,7 +1,17 @@
 Push-Location -Path $PSScriptRoot
 karma start karma.conf.js 
 
-$wc = New-Object 'System.Net.WebClient'
-$wc.UploadFile("https://ci.appveyor.com/api/testresults/mstest/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\Results.trx))
-
+Try
+{
+    $wc = New-Object 'System.Net.WebClient'
+    $wc.UploadFile("https://ci.appveyor.com/api/testresults/mstest/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\Results.trx))
+}
+Catch [System.Net.WebException]
+{
+    echo "Pushing test results failed:" 
+    $responseStream = $_.Exception.Response.GetResponseStream()
+    $sr = New-Object System.IO.StreamReader $responseStream
+    $result = $sr.ReadToEnd()
+    Write-Host $result
+}
 Get-Location 
