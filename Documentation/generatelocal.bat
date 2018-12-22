3133b0c12cdf67c267d8ed3c03b5a456d9efba52
Daniel Kuschny <danielku15+gitkraken@gmail.com>
2018-12-22 02:10:45 +0100

    Updated documentation scripts for latest Wyam version

--- Documentation/generatelocal.bat
+++ Documentation/generatelocal.bat
@@ -3,7 +3,7 @@
 pushd %~dp0
 
 SET GitBranch=%APPVEYOR_REPO_BRANCH%
-SET WYAM=%USERPROFILE%\.nuget\packages\wyam\1.4.1\tools\net462\Wyam.exe
+SET WYAM=%USERPROFILE%\.nuget\packages\wyam\2.1.1\tools\netcoreapp2.1\Wyam.dll
 IF "%GitBranch%"=="" ( 
 	git rev-parse HEAD > head.txt
 	set /p GitRevision=<head.txt
@@ -15,7 +15,7 @@ rmdir /S /Q input\assets\js\alphaTab
 xcopy ..\Build\JavaScript input\assets\js\alphaTab\ /Y /S
 xcopy ..\TestData\Docs input\assets\files\ /Y /S
 
-echo GitRevision=%GitRevision% GitBranch=%GitBranch%
-%WYAM% build --setting GitRevision=%GitRevision% --setting GitBranch=%GitBranch% --setting LinkRoot=/Documentation/output/ --setting NoApi=true
+echo GitRevision=%GitRevision% GitBranch=%GitBranch% LinkRoot=%GitBranch% WYAM=%WYAM%
+dotnet %WYAM% build --setting GitRevision=%GitRevision% --setting GitBranch=%GitBranch% --setting LinkRoot=/Documentation/output/ --setting NoApi=true
 
 popd
