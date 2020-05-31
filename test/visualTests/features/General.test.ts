d1756abd180785d7bf13f29c9c7278d07d1a8c07
Danielku15 <danielku15@coderline.net>
2020-05-31 16:32:45 +0200

    Added test for overall notation elements

--- test/visualTests/features/General.test.ts
+++ test/visualTests/features/General.test.ts
@@ -1,6 +1,9 @@
-import { StaveProfile } from '@src/DisplaySettings';
+import { StaveProfile, LayoutMode } from '@src/DisplaySettings';
 import { Settings } from '@src/Settings';
 import { VisualTestHelper } from '@test/visualTests/VisualTestHelper';
+import { TestPlatform } from '@test/TestPlatform';
+import { ScoreLoader } from '@src/importer/ScoreLoader';
+import { Score } from '@src/model/Score';
 
 describe('GeneralTests', () => {
     it('song-details', async () => {
@@ -21,5 +24,25 @@ describe('GeneralTests', () => {
 
     it('tuning', async () => {
         await VisualTestHelper.runVisualTest('features/general/tuning.gp5');
+    });
+
+    it('notation-legend-default', async () => {
+        let settings: Settings = new Settings();
+        settings.display.layoutMode = LayoutMode.Horizontal;
+        const inputFileData = await TestPlatform.loadFile(`test-data/visual-tests/features/general/notation-legend.gp`);
+        const referenceFileName = `features/general/notation-legend-default.png`;
+        let score: Score = ScoreLoader.loadScoreFromBytes(inputFileData, settings);
+
+        await VisualTestHelper.runVisualTestScore(score, referenceFileName, settings, [0]);
+    });
+
+    it('notation-legend-songbook', async () => {
+        let settings: Settings = new Settings();
+        settings.display.layoutMode = LayoutMode.Horizontal;
+        settings.setSongBookModeSettings();
+        const inputFileData = await TestPlatform.loadFile(`test-data/visual-tests/features/general/notation-legend.gp`);
+        const referenceFileName = `features/general/notation-legend-songbook.png`;
+        let score: Score = ScoreLoader.loadScoreFromBytes(inputFileData, settings);
+        await VisualTestHelper.runVisualTestScore(score, referenceFileName, settings, [0]);
     });
 });
