a2913bd4ff3a00e0d5bce45944fa949838d662bf
Danielku15 <danielku15@coderline.net>
2021-01-05 15:26:08 +0100

    Fix C# version

---
+++ src.csharp/AlphaTab/Core/EcmaScript/Set.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace AlphaTab.Core.EcmaScript
+{
+    public class Set<T>
+    {
+        private readonly HashSet<T> _data = new HashSet<T>();
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Add(T item)
+        {
+            _data.Add(item);
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Has(T item)
+        {
+            return _data.Contains(item);
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            foreach (var i in _data)
+            {
+                action(i);
+            }
+        }
+    }
+}
