1e66dc95d0643b05261aa41cf262236dcf03c194
Danielku15 <danielku15@coderline.net>
2021-01-17 14:45:13 +0100

    Use cast syntax

--- src.csharp/AlphaTab/Core/EcmaScript/Uint8Array.cs
+++ src.csharp/AlphaTab/Core/EcmaScript/Uint8Array.cs
@@ -18,7 +18,7 @@ namespace AlphaTab.Core.EcmaScript
 
         public Uint8Array(IList<double> data)
         {
-            _data = new ArraySegment<byte>(data.Cast<byte>().ToArray());
+            _data = new ArraySegment<byte>(data.Select(d => (byte)d).ToArray());
         }
 
         public Uint8Array(byte[] data)
@@ -32,34 +32,34 @@ namespace AlphaTab.Core.EcmaScript
         }
 
         public Uint8Array(double size)
-            : this(new byte[(int) size])
+            : this(new byte[(int)size])
         {
         }
 
         public Uint8Array(IEnumerable<int> values)
-            : this(values.Select(d => (byte) d).ToArray())
+            : this(values.Select(d => (byte)d).ToArray())
         {
         }
 
         public double this[double index]
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => _data.Array[_data.Offset + (int) index];
+            get => _data.Array[_data.Offset + (int)index];
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            set => _data.Array[_data.Offset + (int) index] = (byte) value;
+            set => _data.Array[_data.Offset + (int)index] = (byte)value;
         }
 
         public Uint8Array Subarray(double begin, double end)
         {
-            return new Uint8Array(new ArraySegment<byte>(_data.Array, _data.Offset + (int) begin,
-                (int) (end - begin)));
+            return new Uint8Array(new ArraySegment<byte>(_data.Array, _data.Offset + (int)begin,
+                (int)(end - begin)));
         }
 
         public void Set(Uint8Array subarray, double pos)
         {
             var buffer = subarray.Buffer.Raw;
-            System.Buffer.BlockCopy(buffer.Array, (int) buffer.Offset, _data.Array,
-                _data.Offset + (int) pos, buffer.Count);
+            System.Buffer.BlockCopy(buffer.Array, (int)buffer.Offset, _data.Array,
+                _data.Offset + (int)pos, buffer.Count);
         }
 
         public static implicit operator Uint8Array(byte[] v)
@@ -69,7 +69,7 @@ namespace AlphaTab.Core.EcmaScript
 
         IEnumerator<double> IEnumerable<double>.GetEnumerator()
         {
-            return _data.Select(d => (double) d).GetEnumerator();
+            return _data.Select(d => (double)d).GetEnumerator();
         }
 
         public IEnumerator<byte> GetEnumerator()
