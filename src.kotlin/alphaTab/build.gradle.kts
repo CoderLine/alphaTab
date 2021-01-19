4246704243f68ac68605da4c04f5e809ed8a7519
Danielku15 <danielku15@coderline.net>
2021-01-19 17:47:04 +0100

    Added kotlin multiplatform project

---
+++ src.kotlin/alphaTab/build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    kotlin("multiplatform") version "1.4.10"
+    id("com.android.library")
+    id("kotlin-android-extensions")
+}
+
+group = "net.alphatab"
+version = "1.3-SNAPSHOT"
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "10"
+        }
+        testRuns["test"].executionTask.configure {
+            useJUnit()
+        }
+    }
+    android()
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+        val jvmMain by getting
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+                implementation("com.google.android.material:material:1.2.1")
+            }
+        }
+        val androidTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+                implementation("junit:junit:4.13")
+            }
+        }
+    }
+}
+
+android {
+    compileSdkVersion(29)
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        minSdkVersion(24)
+        targetSdkVersion(29)
+    }
+}
---
+++ src.kotlin/alphaTab/src/commonMain/kotlin/AlphaTabLib.kt
@@ -0,0 +1,7 @@
+class AlphaTabLib {
+    public fun run(count:Int) {
+        for(i in 0..count) {
+            println("Hello World $i");
+        }
+    }
+}
