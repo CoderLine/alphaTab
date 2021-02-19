bb1e13037c282f2c01b51424b5c14dadc5549d42
Kyle Decot <kyle@joinroot.com>
2021-02-19 11:03:25 -0500

    use-kebab-case

--- test/model/Color.test.ts
+++ test/model/Color.test.ts
@@ -1,7 +1,7 @@
 import { Color } from "@src/model/Color";
 
 describe('ColorTests', () => {
-  it('fromJson rgb', () => {
+  it('fromJson-rgb', () => {
     const color = Color.fromJson('rgb(0,0,0)');
 
     expect(color!.rgba).toEqual('#000000');
