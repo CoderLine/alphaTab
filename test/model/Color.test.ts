56d455020165c89e710d1a274c4d90be7fa6fe46
Kyle Decot <kyle.decot@icloud.com>
2021-02-24 11:51:01 -0500

    Fix color parsing and add Basic Color Test for RGB (#543)

---
+++ test/model/Color.test.ts
@@ -0,0 +1,9 @@
+import { Color } from "@src/model/Color";
+
+describe('ColorTests', () => {
+  it('fromJson-rgb', () => {
+    const color = Color.fromJson('rgb(0,0,0)');
+
+    expect(color!.rgba).toEqual('#000000');
+  });
+});
