51e6724a4d69a5dba5d87ad1153f401cd429e556
sad-xu <1031568754@qq.com>
2022-02-22 23:26:39 +0800

    Fixed color model

--- test/model/Color.test.ts
+++ test/model/Color.test.ts
@@ -1,9 +1,45 @@
-import { Color } from "@src/model/Color";
+import { Color } from '@src/model/Color';
 
 describe('ColorTests', () => {
-  it('fromJson-rgb', () => {
-    const color = Color.fromJson('rgb(0,0,0)');
+    it('fromJson-number', () => {
+        const color = Color.fromJson(-635386);
 
-    expect(color!.rgba).toEqual('#000000');
-  });
+        expect(color!.rgba).toEqual('#f64e06');
+    });
+
+    it('fromJson-#RGB', () => {
+        const color = Color.fromJson('#0f0');
+
+        expect(color!.rgba).toEqual('#00ff00');
+    });
+
+    it('fromJson-#RGBA', () => {
+        const color = Color.fromJson('#0f09');
+
+        expect(color!.rgba).toEqual('rgba(0,255,0,0.6)');
+    });
+
+    it('fromJson-#RRGGBB', () => {
+        const color = Color.fromJson('#f64e06');
+
+        expect(color!.rgba).toEqual('#f64e06');
+    });
+
+    it('fromJson-#RRGGBBAA', () => {
+        const color = Color.fromJson('#f64e0680');
+
+        expect(color!.rgba).toEqual('rgba(246,78,6,0.5)');
+    });
+
+    it('fromJson-rgb', () => {
+        const color = Color.fromJson('rgb(246,78,6)');
+
+        expect(color!.rgba).toEqual('#f64e06');
+    });
+
+    it('fromJson-rgba', () => {
+        const color = Color.fromJson('rgba(246,78,6,128)');
+
+        expect(color!.rgba).toEqual('rgba(246,78,6,0.5)');
+    });
 });
