ba3797eb87bae7f6b109d6ad54b0bb589c987115
Danielku15 <danielku15@coderline.net>
2022-10-16 10:18:18 +0200

    Fix flickering and font loading error

--- src/util/FontLoadingChecker.ts
+++ src/util/FontLoadingChecker.ts
@@ -47,61 +47,91 @@ export class FontLoadingChecker {
             } else {
                 failCounter++;
             }
-
         }, 5000);
 
         Logger.debug('Font', `Start checking for font availablility: ${this._families.join(', ')}`);
 
-        let errorHandler = () => {
+        let errorHandler = (e: unknown) => {
             if (this._families.length > 1) {
-                Logger.debug('Font', `[${this._families[0]}] Loading Failed, switching to ${this._families[1]}`);
+                Logger.debug('Font', `[${this._families[0]}] Loading Failed, switching to ${this._families[1]}`, e);
                 this._families.shift();
                 window.setTimeout(() => {
+                    // tslint:disable-next-line: no-floating-promises
                     checkFont();
                 }, 0);
-            }
-            else {
-                Logger.error('Font', `[${this._originalFamilies.join(',')}] Loading Failed, rendering cannot start`);
+            } else {
+                Logger.error('Font', `[${this._originalFamilies.join(',')}] Loading Failed, rendering cannot start`, e);
                 window.clearInterval(failCounterId);
+                debugger;
             }
         };
 
-        let successHandler = (font:string) => {
-            Logger.debug('Rendering', `[${font}] Font API signaled available`);
+        let successHandler = (font: string) => {
+            Logger.debug('Font', `[${font}] Font API signaled available`);
             this.isFontLoaded = true;
             window.clearInterval(failCounterId);
             (this.fontLoaded as EventEmitterOfT<string>).trigger(this._families[0]);
         };
 
-        let checkFont = () => {
+        let checkFont = async () => {
             // Fast Path: check if one of the specified fonts is already available.
             for (const font of this._families) {
-                if ((document as any).fonts.check('1em ' + font)) {
+                if (await this.isFontAvailable(font, false)) {
                     successHandler(font);
                     return;
                 }
             }
 
             // Slow path: Wait for fonts to be loaded sequentially
-            const promise: Promise<FontFace[]> = (document as any).fonts.load(`1em ${this._families[0]}`);
-            promise.then(
-                () => {
-                    Logger.debug('Font', `[${this._families[0]}] Font API signaled loaded`);
-                    if ((document as any).fonts.check('1em ' + this._families[0])) {
-                        successHandler(this._families[0]);
-                    } else {
-                        errorHandler();
-                    }
-                    return true;
-                },
-                reason => {
-                    errorHandler();
-                });
+            try {
+                await (document as any).fonts.load(`1em ${this._families[0]}`);
+            } catch (e) {
+                errorHandler(e);
+            }
+
+            Logger.debug('Font', `[${this._families[0]}] Font API signaled loaded`);
+            if (await this.isFontAvailable(this._families[0], true)) {
+                successHandler(this._families[0]);
+            } else {
+                errorHandler('Font not available');
+            }
+            return true;
         };
 
-
         (document as any).fonts.ready.then(() => {
+            // tslint:disable-next-line: no-floating-promises
             checkFont();
+        });
+    }
+
+    private isFontAvailable(family: string, advancedCheck: boolean): Promise<boolean> {
+        return new Promise<boolean>(resolve => {
+            // In some very rare occasions Chrome reports false for the font.
+            // in this case we try to force some refresh and reload by creating an element with this font.
+            const fontString = '1em ' + family;
+            if ((document as any).fonts.check(fontString)) {
+                resolve(true);
+            } else if (advancedCheck) {
+                Logger.debug('Font', `Font ${family} not available, creating test element to trigger load`);
+                const testElement = document.createElement('div');
+                testElement.style.font = fontString;
+                testElement.style.opacity = '0';
+                testElement.style.position = 'absolute';
+                testElement.style.top = '0';
+                testElement.style.left = '0';
+                testElement.innerText = `Trigger ${family} load`;
+                document.body.appendChild(testElement);
+                setTimeout(() => {
+                    document.body.removeChild(testElement);
+                    if ((document as any).fonts.check(fontString)) {
+                        resolve(true);
+                    } else {
+                        resolve(false);
+                    }
+                }, 200);
+            } else {
+                resolve(false);
+            }
         });
     }
 }
