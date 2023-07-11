c3621f886b0e00251c1034e776de63960e305e39
Danielku15 <danielku15@coderline.net>
2023-07-11 06:25:56 +0200

    Handle missing effect bands

--- src/rendering/EffectBand.ts
+++ src/rendering/EffectBand.ts
@@ -110,14 +110,14 @@ export class EffectBand extends Glyph {
                             // load the effect from the previous renderer if possible.
                             let previousRenderer: EffectBarRenderer = this.renderer
                                 .previousRenderer as EffectBarRenderer;
-                            let previousBand = previousRenderer.getBand(prevBeat.voice.index, this.info.effectId);
+                            let previousBand = previousRenderer.getBand(prevBeat.voice, this.info.effectId);
                             // it can happen that we have an empty voice and then we don't have an effect band 
                             if(previousBand) {
                                 let voiceGlyphs: Map<number, EffectGlyph> = previousBand._effectGlyphs[prevBeat.voice.index];
                                 if (voiceGlyphs.has(prevBeat.index)) {
                                     prevEffect = voiceGlyphs.get(prevBeat.index)!;
                                 }
-                            }                            
+                            }
                         }
                         // if the effect cannot be expanded, create a new glyph
                         // in case of expansion also create a new glyph, but also link the glyphs together
