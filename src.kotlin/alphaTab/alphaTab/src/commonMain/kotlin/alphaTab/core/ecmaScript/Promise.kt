ba24e9b25f4b2b2de838316dbead8a4b10fba0a3
Danielku15 <danielku15@coderline.net>
2023-12-03 12:28:37 +0100

    Implement missing Promise.race

--- src.kotlin/alphaTab/alphaTab/src/commonMain/kotlin/alphaTab/core/ecmaScript/Promise.kt
+++ src.kotlin/alphaTab/alphaTab/src/commonMain/kotlin/alphaTab/core/ecmaScript/Promise.kt
@@ -1,9 +1,19 @@
 package alphaTab.core.ecmaScript
 
+import kotlinx.coroutines.cancelChildren
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.selects.select
+
 internal class Promise {
     companion object {
-        fun <T> race(promises: alphaTab.collections.List<kotlinx.coroutines.Deferred<T>>) {
-
+        suspend fun <T> race(promises: alphaTab.collections.List<kotlinx.coroutines.Deferred<T>>) {
+            return coroutineScope {
+                select {
+                    for(p in promises) {
+                        p.onAwait { it }
+                    }
+                }.also { coroutineContext.cancelChildren() }
+            }
         }
     }
 }
