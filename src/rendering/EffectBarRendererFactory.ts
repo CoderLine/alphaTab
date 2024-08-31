74b0ee6fdf389cc41f044aba72ea38dd23ec27e7
Danielku15 <danielku15@coderline.net>
2024-08-31 21:48:49 +0200

    build: fix Kotlin compilation

--- src/rendering/EffectBarRendererFactory.ts
+++ src/rendering/EffectBarRendererFactory.ts
@@ -14,7 +14,7 @@ export class EffectBarRendererFactory extends BarRendererFactory {
         return this._staffId;
     }
 
-    public shouldShow?: (track:Track, staff:Staff) => boolean;
+    public shouldShow: ((track:Track, staff:Staff) => boolean) | null;
 
     public override getStaffPaddingTop(staff: RenderStaff): number {
         return staff.system.layout.renderer.settings.display.effectStaffPaddingTop;
@@ -24,7 +24,7 @@ export class EffectBarRendererFactory extends BarRendererFactory {
         return staff.system.layout.renderer.settings.display.effectStaffPaddingBottom;
     }
 
-    public constructor(staffId: string, infos: EffectBarRendererInfo[], shouldShow?: (track:Track, staff:Staff) => boolean) {
+    public constructor(staffId: string, infos: EffectBarRendererInfo[], shouldShow: ((track:Track, staff:Staff) => boolean) | null = null) {
         super();
         this.infos = infos;
         this._staffId = staffId;
