bfaafcc42c06268a0484adf99922d9b48600c8ea
Danielku15 <danielku15@coderline.net>
2025-02-13 23:18:56 +0100

    feat(gp3-6): Add beat flags decoding
    beaming mode, beam direction and ottavia

--- src/importer/Gp3To5Importer.ts
+++ src/importer/Gp3To5Importer.ts
@@ -8,7 +8,7 @@ import { IReadable } from '@src/io/IReadable';
 import { AccentuationType } from '@src/model/AccentuationType';
 import { Automation, AutomationType } from '@src/model/Automation';
 import { Bar } from '@src/model/Bar';
-import { Beat } from '@src/model/Beat';
+import { Beat, BeatBeamingMode } from '@src/model/Beat';
 import { BendPoint } from '@src/model/BendPoint';
 import { BrushType } from '@src/model/BrushType';
 import { Chord } from '@src/model/Chord';
@@ -44,6 +44,8 @@ import { Tuning } from '@src/model/Tuning';
 import { FadeType } from '@src/model/FadeType';
 import { Rasgueado } from '@src/model/Rasgueado';
 import { Direction } from '@src/model/Direction';
+import { BeamDirection } from '@src/rendering/utils/BeamDirection';
+import { Ottavia } from '@src/model';
 
 export class Gp3To5Importer extends ScoreImporter {
     private static readonly VersionString: string = 'FICHIER GUITAR PRO ';
@@ -616,13 +618,75 @@ export class Gp3To5Importer extends ScoreImporter {
             }
         }
         if (this._versionNumber >= 500) {
-            this.data.readByte();
-            let flag: number = this.data.readByte();
-            if ((flag & 0x08) !== 0) {
-                this.data.readByte();
-            }
-        }
+            // not 100% sure about the bits here but they look good in all test files.
 
+            const flags = IOHelper.readInt16LE(this.data);
+
+            // beam flags indicate how to handle beams connected to the previous beat,
+            // so we have to set the beaming mode on the previous beat!
+
+            // 1 - Break Beams
+            if ((flags & 0x01) !== 0) {
+                if (newBeat.index > 0) {
+                    voice.beats[newBeat.index - 1].beamingMode = BeatBeamingMode.ForceSplitToNext;
+                }
+            }
+
+            // 2 - Force beams down
+            // this bit also set if we 'invert' a down-stem, but bit 8 will force the direction to up as both bits are set
+            if ((flags & 0x02) !== 0) {
+                newBeat.preferredBeamDirection = BeamDirection.Down;
+            }
+
+            // 4 - Force Beams
+            if ((flags & 0x04) !== 0) {
+                if (newBeat.index > 0) {
+                    voice.beats[newBeat.index - 1].beamingMode = BeatBeamingMode.ForceMergeWithNext;
+                }
+            }
+
+            // 8 - Force beams up
+            if ((flags & 0x08) !== 0) {
+                newBeat.preferredBeamDirection = BeamDirection.Up;
+            }
+
+            // 16 - Ottava 8va
+            if ((flags & 0x10) !== 0) {
+                newBeat.ottava = Ottavia._8va;
+            }
+
+            // 32 - Ottava 8vb
+            if ((flags & 0x20) !== 0) {
+                newBeat.ottava = Ottavia._8vb;
+            }
+
+            // 64 - Ottava 15ma
+            if ((flags & 0x40) !== 0) {
+                newBeat.ottava = Ottavia._15ma;
+            }
+
+            // 128 - Unknown, upper bit of first byte, maybe a placeholder.
+
+            // 256 - Ottava 15mb
+            if ((flags & 0x100) !== 0) {
+                newBeat.ottava = Ottavia._15mb;
+            }
+
+            // 512 - Unknown
+            // 1024 - Unknown
+
+            // 2048 - Break Secondary Beams info set? -> read another byte for flag
+            if ((flags & 0x800) !== 0) {
+                const breakSecondaryBeams = this.data.readByte() != 0;
+                if (newBeat.index > 0 && breakSecondaryBeams) {
+                    voice.beats[newBeat.index - 1].beamingMode = BeatBeamingMode.ForceSplitOnSecondaryToNext;
+                }
+            }
+
+            // 4096 - Unknown
+            // 8096 - Force Tuplet Bracket
+        }
+           
         if (
             beatTextAsLyrics &&
             !newBeat.isRest &&
