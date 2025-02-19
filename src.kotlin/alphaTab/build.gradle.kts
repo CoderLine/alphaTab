a6058146342f6792e0f82d9cb49efcfc5cfdde6f
Danielku15 <danielku15@coderline.net>
2025-02-19 23:36:26 +0100

    build: set sonatype staging profile ID

--- src.kotlin/alphaTab/build.gradle.kts
+++ src.kotlin/alphaTab/build.gradle.kts
@@ -29,11 +29,16 @@ fun loadSetting(envKey: String, propKey: String, setter: (value: String) -> Unit
 
 var ossrhUsername = ""
 var ossrhPassword = ""
+var sonatypeStagingProfileId = ""
 loadSetting("OSSRH_USERNAME", "ossrhUsername") { ossrhUsername = it }
 loadSetting("OSSRH_PASSWORD", "ossrhPassword") { ossrhPassword = it }
+loadSetting("SONATYPE_STAGING_PROFILE_ID", "sonatypeStagingProfileId") {
+    sonatypeStagingProfileId = it
+}
 
 nexusPublishing {
     repositories.sonatype {
+        stagingProfileId = sonatypeStagingProfileId
         nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
         snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
         username = ossrhUsername
