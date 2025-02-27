2679cc95c6250ac7d6f2a481073800f1a727104c
Danielku15 <danielku15@coderline.net>
2025-02-27 17:48:28 +0100

    refactor: Integrate Noto Fonts (and move files to LFS)

--- test-data/bundler/webpack/src/app.mjs
+++ test-data/bundler/webpack/src/app.mjs
@@ -1,30 +1,3 @@
-import * as alphaTab from '../../../../dist/alphaTab.mjs'
-
-const toolbar = document.createElement('div');
-toolbar.style.position = 'absolute';
-toolbar.style.top = "0px";
-toolbar.style.left = "0px";
-toolbar.style.right = "0px";
-toolbar.style.height = "20px";
-
-
-
-const element = document.querySelector('#alphaTab');
-const api = new alphaTab.AlphaTabApi(element, {
-  core: {
-    file: "https://www.alphatab.net/files/canon.gp",
-    // Resolve URL as we copied the files
-    fontDirectory: new URL("/font/bravura/", document.location).href,
-    logLevel: alphaTab.LogLevel.Debug
-  },
-  player: {
-    enablePlayer: true,
-    // Resolve URL as we copied the files
-    soundFont: new URL("/font/sonivox/sonivox.sf2", document.location).href
-  },
-});
-
-const playPause = document.querySelector('#playPause');
-playPause.onclick = ()=>{
-  api.playPause();
-};
+version https://git-lfs.github.com/spec/v1
+oid sha256:7bf8fab7d8b81fa188583b762e153008ac808cab0c746e6f4dffb179285d9a37
+size 876
