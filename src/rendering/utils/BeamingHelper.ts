9a84abef1d2e15c16fbeb4d26ce4195a3ca963fa
Danielku15 <danielku15@coderline.net>
2025-03-09 16:24:02 +0100

    fix: changed alignment for deadslapped

--- src/rendering/utils/BeamingHelper.ts
+++ src/rendering/utils/BeamingHelper.ts
@@ -108,7 +108,7 @@ export class BeamingHelper {
     }
 
     private beatHasFlag(beat: Beat) {
-        return !beat.isRest && (beat.duration > Duration.Quarter || beat.graceType !== GraceType.None);
+        return !beat.deadSlapped && !beat.isRest && (beat.duration > Duration.Quarter || beat.graceType !== GraceType.None);
     }
 
     public constructor(staff: Staff, renderer: BarRendererBase) {
