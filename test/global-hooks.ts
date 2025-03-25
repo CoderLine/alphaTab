595831c57d0f92abfc9cbe858e91b17c5552a055
Danielku15 <danielku15@coderline.net>
2025-03-25 20:06:05 +0100

    test: Add Snapshot testing via jest-snapshot
    # Conflicts:
    #	package-lock.json

--- test/global-hooks.ts
+++ test/global-hooks.ts
@@ -1,8 +1,20 @@
 /** @target web */
 import * as chai from 'chai';
+import { afterAll, beforeEachTest, initializeJestSnapshot } from './mocha.jest-snapshot';
 
 export const mochaHooks = {
-    beforeAll() {
+    beforeAll: async function () {
         chai.config.truncateThreshold = 0; // disable truncating
+        await initializeJestSnapshot();
+    },
+
+    beforeEach: function (done) {
+        beforeEachTest(this.currentTest!);
+        done();
+    },
+
+    afterAll: function (done) {
+        afterAll();
+        done();
     }
-};
+} satisfies Mocha.RootHookObject;
