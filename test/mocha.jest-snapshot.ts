2fd3c3267b20a12721d362a07a7da7d0e89cc5ef
Danielku15 <danielku15@coderline.net>
2025-03-27 13:56:12 +0100

    test: Minimize score snapshots

--- test/mocha.jest-snapshot.ts
+++ test/mocha.jest-snapshot.ts
@@ -3,7 +3,7 @@ import chalk, { supportsColor } from 'chalk';
 import * as chai from 'chai';
 import url from 'node:url';
 import path from 'node:path';
-import { JsonConverter, Score } from '@src/model';
+import { Bar, Beat, JsonConverter, MasterBar, Note, Score, Staff, Track, Voice } from '@src/model';
 import { addSerializer, buildSnapshotResolver, SnapshotResolver, SnapshotState, toMatchSnapshot } from 'jest-snapshot';
 import { Config } from '@jest/types';
 import slash from 'slash';
@@ -11,6 +11,14 @@ import type { SyncExpectationResult } from 'expect';
 import { equals, iterableEquality, subsetEquality } from '@jest/expect-utils';
 import * as matcherUtils from 'jest-matcher-utils';
 import { AssertionError } from 'assertion-error';
+import { ScoreSerializer } from '@src/generated/model/ScoreSerializer';
+import { MasterBarSerializer } from '@src/generated/model/MasterBarSerializer';
+import { TrackSerializer } from '@src/generated/model/TrackSerializer';
+import { StaffSerializer } from '@src/generated/model/StaffSerializer';
+import { BarSerializer } from '@src/generated/model/BarSerializer';
+import { VoiceSerializer } from '@src/generated/model/VoiceSerializer';
+import { BeatSerializer } from '@src/generated/model/BeatSerializer';
+import { NoteSerializer } from '@src/generated/model/NoteSerializer';
 
 // Mocha and Chai integration (called from global-hooks.ts)
 declare global {
@@ -25,12 +33,14 @@ export async function initializeJestSnapshot() {
     console.log(supportsColor);
 
     // Setup jest-snapshot
+
     addSerializer({
         test(val) {
             return val instanceof Score;
         },
         serialize(val, config, indentation, depth, refs, printer) {
             const json = JsonConverter.scoreToJsObject(val);
+            filterOutDefaultValues(json as Map<string, unknown>);
             return printer(json, config, indentation, depth, refs);
         }
     });
@@ -357,7 +367,6 @@ function writeSummaryReport() {
 
     snapshots.uncheckedKeysByFile = snapshots.uncheckedKeysByFile.filter(f => f.keys.length > 0);
 
-
     const summary: string[] = [];
     summary.push(SNAPSHOT_SUMMARY('Snapshot Summary'));
     if (snapshots.added) {
@@ -440,5 +449,143 @@ function writeSummaryReport() {
     console.log();
     for (const line of summary) {
         console.log(line);
+    }
+}
+
+//
+// Score Snapshot -> Filtering out any default values from the tree for simpler snapshots
+
+// we create empty basic objects and reset some props we always want in the snapshot
+const defaultScore = new Score();
+const defaultMasterBar = new MasterBar();
+defaultMasterBar.index = -1;
+
+const defaultTrack = new Track();
+defaultTrack.index = -1;
+
+const defaultStaff = new Staff();
+defaultStaff.index = -1;
+
+const defaultBar = new Bar();
+defaultBar.id = -1;
+defaultBar.index = -1;
+
+const defaultVoice = new Voice();
+defaultVoice.id = -1;
+defaultVoice.index = -1;
+
+const defaultBeat = new Beat();
+defaultBeat.id = -1;
+defaultBeat.index = -1;
+
+const defaultNote = new Note();
+defaultNote.id = -1;
+defaultNote.index = -1;
+
+const defaultScoreJson = ScoreSerializer.toJson(defaultScore)!;
+const defaultMasterBarJson = MasterBarSerializer.toJson(defaultMasterBar)!;
+const defaultTrackJson = TrackSerializer.toJson(defaultTrack)!;
+const defaultStaffJson = StaffSerializer.toJson(defaultStaff)!;
+const defaultBarJson = BarSerializer.toJson(defaultBar)!;
+const defaultVoiceJson = VoiceSerializer.toJson(defaultVoice)!;
+const defaultBeatJson = BeatSerializer.toJson(defaultBeat)!;
+const defaultNoteJson = NoteSerializer.toJson(defaultNote)!;
+
+function filterOutDefaultValues(scoreJson: Map<string, unknown>) {
+    const masterBars = scoreJson.get('masterbars') as Map<string, unknown>[];
+    for (const masterBar of masterBars) {
+        sanitizeJson(masterBar, defaultMasterBarJson, 'MasterBar');
+    }
+
+    const tracks = scoreJson.get('tracks') as Map<string, unknown>[];
+    for (const track of tracks) {
+        const staves = track.get('staves') as Map<string, unknown>[];
+        for (const staff of staves) {
+            const bars = staff.get('bars') as Map<string, unknown>[];
+            for (const bar of bars) {
+                const voices = bar.get('voices') as Map<string, unknown>[];
+                for (const voice of voices) {
+                    const beats = voice.get('beats') as Map<string, unknown>[];
+                    for (const beat of beats) {
+                        const notes = beat.get('notes') as Map<string, unknown>[];
+                        for (const note of notes) {
+                            sanitizeJson(note, defaultNoteJson, 'Note');
+                        }
+                        sanitizeJson(beat, defaultBeatJson, 'Beat');
+                    }
+                    sanitizeJson(voice, defaultVoiceJson, 'Voice');
+                }
+                sanitizeJson(bar, defaultBarJson, 'Bar');
+            }
+            sanitizeJson(staff, defaultStaffJson, 'Staff');
+        }
+        sanitizeJson(track, defaultTrackJson, 'Track');
+    }
+
+    // walk hierarchy and filter out stuff
+    sanitizeJson(scoreJson, defaultScoreJson, 'Score');
+}
+
+function sanitizeJson(modelJson: Map<string, unknown>, defaultValueJson: Map<string, unknown>, kind?: string) {
+    // add a __kind property to identify the objects easier in the snap files
+    if (kind) {
+        modelJson.set('__kind', kind);
+    }
+
+    for (const [k, dv] of defaultValueJson) {
+        if (modelJson.has(k)) {
+            const v = modelJson.get(k);
+
+            let isEqual = false;
+            if (typeof v === typeof dv) {
+                switch (typeof dv) {
+                    case 'string':
+                        isEqual = (v as string) === (dv as string);
+                        break;
+                    case 'number':
+                        isEqual = (v as number) === (dv as number);
+                        break;
+                    case 'bigint':
+                        isEqual = (v as bigint) === (dv as bigint);
+                        break;
+                    case 'boolean':
+                        isEqual = (v as boolean) === (dv as boolean);
+                        break;
+                    case 'undefined':
+                        isEqual = (v as undefined) === (dv as undefined);
+                        break;
+                    case 'object':
+                        if (dv === null && v === null) {
+                            isEqual = true;
+                        } else if (Array.isArray(dv) && Array.isArray(v)) {
+                            isEqual = dv.length === 0 && v.length === 0;
+                        } else if (dv instanceof Map && v instanceof Map) {
+                            sanitizeJson(v as Map<string, unknown>, dv as Map<string, unknown>);
+                            if (v.size === 0) {
+                                isEqual = true;
+                            }
+                        } else {
+                            // we should not have any other types in our JSONs, if we extend it, this will catch it
+                            throw new Error('Unexpected value in serialized json' + String(dv));
+                        }
+                        break;
+                }
+            }
+
+            if (isEqual) {
+                modelJson.delete(k);
+            }
+        }
+    }
+
+    // "sort" __kind to top.
+    if (kind) {
+        for (const key of Array.from(modelJson.keys())) {
+            if (key !== '__kind') {
+                const v = modelJson.get(key);
+                modelJson.delete(key);
+                modelJson.set(key, v);
+            }
+        }
     }
 }
