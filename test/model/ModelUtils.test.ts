9c9719343b8f72bcf7964c11a63e0800854c2a66
Danielku15 <danielku15@coderline.net>
2025-06-09 23:01:32 +0200

    test: add tests for trimming

--- test/model/ModelUtils.test.ts
+++ test/model/ModelUtils.test.ts
@@ -1,6 +1,42 @@
+import { AlphaTexImporter } from '@src/importer/AlphaTexImporter';
+import { Settings } from '@src/Settings';
+import { expect } from 'chai';
+
 describe('ModelUtils', () => {
-    it('trimEmptyBarsAtEnd', () => {
-        // create a variety of test files with empty and non-empty bars at end
-        // check trimming accordingly.
+    function trimTest(tex: string, expectedBars: number) {
+        const importer = new AlphaTexImporter();
+        importer.initFromString(tex, new Settings());
+
+        const score = importer.readScore();
+        expect(score.masterBars.length).to.equal(expectedBars);
+    }
+
+    it('trimEmptyBarsAtEndFullyEmpty', () => {
+        trimTest('. | | | | | ', 1);
+    });
+
+    it('trimEmptyBarsAtEndSomeSet', () => {
+        trimTest('C4 | C4 | C4 | | | ', 3);
+    });
+
+    it('trimEmptyBarsAtEndMultiTrackMixed', () => {
+        trimTest(
+            `
+            \\track T1
+            C4 | C4 | C4 | C4 | | | |
+            \\track T2
+            C4 | C4 | C4 | C4 | C4 | | |    
+        `,
+            5
+        );
+    });
+
+    it('trimEmptyBarsAtEndNonContentChange', () => {
+        trimTest(
+            `
+            C4 | C4 | \\tempo 80 | \\ts 3 4 | | | |
+        `,
+            4
+        );
     });
 });
