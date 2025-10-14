2d7993abcecab3610c3c9415f8d788325c771aee
Danielku15 <danielku15@coderline.net>
2025-10-15 00:49:36 +0900

    build: fix kotlin

--- packages/kotlin/src/android/src/main/java/alphaTab/core/IAlphaTabEnum.kt
+++ packages/kotlin/src/android/src/main/java/alphaTab/core/IAlphaTabEnum.kt
@@ -4,11 +4,8 @@ interface IAlphaTabEnum {
     val value: Int
 }
 
-interface IAlphaTabEnumCompanion<T> {
-    val values:Array<T>
-    fun fromValue(value:Double): T
-}
 
 interface IAlphaTabEnumCompanion<T> {
+    val values:Array<T>
     fun fromValue(value:Double): T
 }
