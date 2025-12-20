7cd9618a44ee2fac52b07669c3e83ddec33c01c9
Danielku15 <danielku15@coderline.net>
2025-12-20 14:41:29 +0100

    fix(dotnet): UI thread dispatching

--- packages/csharp/src/AlphaTab.Windows/Wpf/FrameworkElementContainer.cs
+++ packages/csharp/src/AlphaTab.Windows/Wpf/FrameworkElementContainer.cs
@@ -46,95 +46,119 @@ namespace AlphaTab.Wpf
 
         public double Width
         {
-            get => (float) Control.ActualWidth;
-            set => Control.Width = value;
+            get => (float)Control.ActualWidth;
+            set => Control.Dispatcher.BeginInvoke((Action)(() => { Control.Width = value; }));
         }
 
 
         public double Height
         {
-            get => (float) Control.ActualHeight;
-            set => Control.Height = value;
+            get => (float)Control.ActualHeight;
+            set => Control.Dispatcher.BeginInvoke((Action)(() => { Control.Height = value; }));
         }
 
         public bool IsVisible => Control.IsVisible && Control.ActualWidth > 0;
 
         public double ScrollLeft
         {
-            get => Control is ScrollViewer scroll ? (float) scroll.HorizontalOffset : 0;
+            get => Control is ScrollViewer scroll ? (float)scroll.HorizontalOffset : 0;
 
             set
             {
-                if (Control is ScrollViewer scroll)
+                Control.Dispatcher.BeginInvoke((Action)(() =>
                 {
-                    scroll.ScrollToHorizontalOffset(value);
-                }
+                    if (Control is ScrollViewer scroll)
+                    {
+                        scroll.ScrollToHorizontalOffset(value);
+                    }
+                }));
             }
         }
 
         public double ScrollTop
         {
-            get => Control is ScrollViewer scroll ? (float) scroll.VerticalOffset : 0;
+            get => Control is ScrollViewer scroll ? (float)scroll.VerticalOffset : 0;
 
             set
             {
-                if (Control is ScrollViewer scroll)
+                Control.Dispatcher.BeginInvoke((Action)(() =>
                 {
-                    scroll.ScrollToVerticalOffset(value);
-                }
+                    if (Control is ScrollViewer scroll)
+                    {
+                        scroll.ScrollToVerticalOffset(value);
+                    }
+                }));
             }
         }
 
         public void AppendChild(IContainer child)
         {
-            if (Control is Panel p)
+            Control.Dispatcher.BeginInvoke((Action)(() =>
             {
-                p.Children.Add(((FrameworkElementContainer) child).Control);
-            }
-            else if (Control is ScrollViewer s && s.Content is ContentControl sc)
-            {
-                sc.Content = ((FrameworkElementContainer) child).Control;
-            }
-            else if (Control is ScrollViewer ss && ss.Content is Decorator d)
-            {
-                d.Child = ((FrameworkElementContainer) child).Control;
-            }
-            else if (Control is ScrollViewer sss && sss.Content is Panel pp)
-            {
-                pp.Children.Add(((FrameworkElementContainer) child).Control);
-            }
-            else if (Control is ContentControl c)
-            {
-                c.Content = ((FrameworkElementContainer) child).Control;
-            }
+                if (Control is Panel p)
+                {
+                    p.Children.Add(((FrameworkElementContainer)child).Control);
+                }
+                else if (Control is ScrollViewer s && s.Content is ContentControl sc)
+                {
+                    sc.Content = ((FrameworkElementContainer)child).Control;
+                }
+                else if (Control is ScrollViewer ss && ss.Content is Decorator d)
+                {
+                    d.Child = ((FrameworkElementContainer)child).Control;
+                }
+                else if (Control is ScrollViewer sss && sss.Content is Panel pp)
+                {
+                    pp.Children.Add(((FrameworkElementContainer)child).Control);
+                }
+                else if (Control is ContentControl c)
+                {
+                    c.Content = ((FrameworkElementContainer)child).Control;
+                }
+            }));
         }
 
 
+        private double _targetX = 0;
         public void StopAnimation()
         {
-            Control.BeginAnimation(Canvas.LeftProperty, null);
+            Control.Dispatcher.BeginInvoke((Action)(() =>
+            {
+                Control.BeginAnimation(Canvas.LeftProperty, null);
+                Canvas.SetLeft(Control, _targetX);
+            }));
         }
 
         public void TransitionToX(double duration, double x)
         {
-            Control.BeginAnimation(Canvas.LeftProperty,
-                new DoubleAnimation(x, new Duration(TimeSpan.FromMilliseconds(duration))));
+            _targetX = x;
+            Control.Dispatcher.BeginInvoke((Action)(() =>
+            {
+                Control.BeginAnimation(Canvas.LeftProperty,
+                    new DoubleAnimation(x, new Duration(TimeSpan.FromMilliseconds(duration))));
+            }));
         }
 
         public void Clear()
         {
-            if (Control is Panel p)
+            Control.Dispatcher.BeginInvoke((Action)(() =>
             {
-                p.Children.Clear();
-            }
+                if (Control is Panel p)
+                {
+                    p.Children.Clear();
+                }
+            }));
         }
 
         public void SetBounds(double x, double y, double w, double h)
         {
-            Canvas.SetLeft(Control, x);
-            Canvas.SetTop(Control, y);
-            Control.Width = w;
-            Control.Height = h;
+            Control.Dispatcher.BeginInvoke((Action)(() =>
+            {
+                Canvas.SetLeft(Control, x);
+                Canvas.SetTop(Control, y);
+                Control.Width = w;
+                Control.Height = h;
+            }));
         }
 
         public IEventEmitter Resize { get; set; }
