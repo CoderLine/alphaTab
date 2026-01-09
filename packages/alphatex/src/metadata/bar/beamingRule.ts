d38cc160c0f966e9c5c80c4e843672db31621624
danielku15 <danielku15@gmail.com>
2026-01-09 04:10:33 +0100

    docs: extend description for writing beaming rules

--- packages/alphatex/src/metadata/bar/beamingRule.ts
+++ packages/alphatex/src/metadata/bar/beamingRule.ts
@@ -5,9 +5,29 @@ export const beaming: MetadataTagDefinition = {
     tag: '\\beaming',
     snippet: '\\beaming ($1 $2) $0',
     shortDescription: 'Set the time signature',
-    // TODO: longer description on how to define beams
     longDescription: `
     Defines a custom beaming rule defining how beams of certain durations should be beamed.
+
+    To define how beats should be beamed we need 2 parts:
+
+    1. A duration with which we splitup the bars
+    2. A list of group sizes defining how many split-parts should be beamed together. 
+
+    The beaming rules go hand-in-hand with the time signature as the rules need to properly
+    define the groups for the whole beat. 
+
+    Let's take a simple example of a 4/4 time signature. If we want to ensure that the beats within the quarter notes are 
+    beamed together we can write variants like this:
+
+    a. \`\\beaming (4 1 1 1 1)\`
+    b. \`\\beaming (8 2 2 2 2)\`
+    c. \`\\beaming (16 4 4 4 4)\`
+
+    We slice the bar into 4, 8 or 16 parts. Then we add "groups" to those parts. If two beats start in the same group, they can be beamed together.
+    Simple as that. 
+
+    There are some common guidelines on how beaming "should be done" and alphaTab ships a wide range of defaults. But in case of more specialized time signatures,
+    you can also customize the beaming as you need by slicing the bar and grouping the beats as needed.
     `,
     signatures: [
         {
